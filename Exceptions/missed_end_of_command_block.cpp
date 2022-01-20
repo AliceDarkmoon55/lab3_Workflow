ff28872217399f64056b8713b5e6a909c26aacb4
AliceDarkmoon55 <44077088+AliceDarkmoon55@users.noreply.github.com>
2022-01-20 18:38:49 +0700

    Create missed_end_of_command_block.cpp

---
+++ Exceptions/missed_end_of_command_block.cpp
@@ -0,0 +1,8 @@
+#include "missed_end_of_command_block.h"
+
+error::missed_end_of_command_block::missed_end_of_command_block(const std::string &error_message) : err_msg(error_message)
+{}
+
+const char *error::missed_end_of_command_block::what() const noexcept {
+    return err_msg.c_str();
+}
