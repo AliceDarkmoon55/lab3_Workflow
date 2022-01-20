6081875b093d49ab1be4f80c91f0a5f27180f91d
AliceDarkmoon55 <44077088+AliceDarkmoon55@users.noreply.github.com>
2022-01-20 18:40:14 +0700

    Create missed_start_of_command_block.cpp

---
+++ Exceptions/missed_start_of_command_block.cpp
@@ -0,0 +1,8 @@
+#include "missed_start_of_command_block.h"
+
+error::missed_start_of_command_block::missed_start_of_command_block(const std::string &error_message) : err_msg(error_message)
+{}
+
+const char *error::missed_start_of_command_block::what() const noexcept {
+    return err_msg.c_str();
+}
