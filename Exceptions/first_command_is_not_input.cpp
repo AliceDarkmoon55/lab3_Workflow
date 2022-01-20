8e803f561e6ce46ebf7b7c3f0819879b33d9f417
AliceDarkmoon55 <44077088+AliceDarkmoon55@users.noreply.github.com>
2022-01-20 18:34:27 +0700

    Create first_command_is_not_input.cpp

---
+++ Exceptions/first_command_is_not_input.cpp
@@ -0,0 +1,8 @@
+#include "first_command_is_not_input.h"
+
+error::first_command_is_not_input::first_command_is_not_input(const std::string &error_message) : err_msg(error_message)
+{}
+
+const char *error::first_command_is_not_input::what() const noexcept {
+    return err_msg.c_str();
+}
