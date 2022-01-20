2aac51fcd9063a2e80bd8055d2073b0ff6503815
AliceDarkmoon55 <44077088+AliceDarkmoon55@users.noreply.github.com>
2022-01-20 18:41:47 +0700

    Create multiply_makers.cpp

---
+++ Exceptions/multiply_makers.cpp
@@ -0,0 +1,8 @@
+#include "multiply_makers.h"
+
+error::multiply_makers::multiply_makers(const std::string &error_message) : err_msg(error_message)
+{}
+
+const char *error::multiply_makers::what() const noexcept {
+    return err_msg.c_str();
+}
