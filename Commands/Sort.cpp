d0f79f7e60d70d9c4f4f6a6a4310f948a223cb92
AliceDarkmoon55 <44077088+AliceDarkmoon55@users.noreply.github.com>
2022-01-20 19:04:30 +0700

    Create Sort.cpp

---
+++ Commands/Sort.cpp
@@ -0,0 +1,21 @@
+#include "Sort.h"
+#include <algorithm>
+#include "../BlockMaker.h"
+#include "../Exceptions/wrong_arguments_amount.h"
+
+static BlockMaker<Sort> maker("sort");
+
+Sort::Sort(std::vector<std::string> &args) {
+    if (!args.empty()){
+        throw error::wrong_arguments_amount("Wrong arguments for command <sort>>!");
+    }
+}
+
+std::vector<std::string> Sort::execute(std::vector<std::string>& data) {
+    std::sort(data.begin(), data.end());
+    return data;
+}
+
+COMMAND_TYPE Sort::get_command_type() const noexcept {
+    return COMMAND_TYPE::PROCESSING;
+}
