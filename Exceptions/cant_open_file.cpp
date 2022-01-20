5f3ac00ea7b66e52cbaf9750d8a98a5b43e0ea20
AliceDarkmoon55 <44077088+AliceDarkmoon55@users.noreply.github.com>
2022-01-20 18:31:13 +0700

    Create cant_open_file.cpp

---
+++ Exceptions/cant_open_file.cpp
@@ -0,0 +1,8 @@
+#include "cant_open_file.h"
+
+error::cant_open_file::cant_open_file(const std::string &error_message) : err_msg(error_message)
+{}
+
+const char *error::cant_open_file::what() const noexcept {
+    return err_msg.c_str();
+}
