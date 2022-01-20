62443bca1e39893f87da2ee3e4650cb104f7ee77
AliceDarkmoon55 <44077088+AliceDarkmoon55@users.noreply.github.com>
2022-01-20 18:47:09 +0700

    Create wrong_command_order.cpp

---
+++ Exceptions/wrong_command_order.cpp
@@ -0,0 +1,8 @@
+#include "wrong_command_order.h"
+
+error::wrong_command_order::wrong_command_order(const std::string &error_message) : err_msg(error_message)
+{}
+
+const char *error::wrong_command_order::what() const noexcept {
+    return err_msg.c_str();
+}
