d4fae37556a0d41c47068a0bfe079d260f189b85
AliceDarkmoon55 <44077088+AliceDarkmoon55@users.noreply.github.com>
2022-01-20 18:44:35 +0700

    Create wrong_arguments_amount.cpp

---
+++ Exceptions/wrong_arguments_amount.cpp
@@ -0,0 +1,8 @@
+#include "wrong_arguments_amount.h"
+
+error::wrong_arguments_amount::wrong_arguments_amount(const std::string &error_message) : err_msg(error_message)
+{}
+
+const char* error::wrong_arguments_amount::what() const noexcept{
+    return err_msg.c_str();
+}
