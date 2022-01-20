166631a26845a7f0a5a986be396888385fb5c3f6
AliceDarkmoon55 <44077088+AliceDarkmoon55@users.noreply.github.com>
2022-01-20 18:48:34 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
+#include <stdexcept>
 #include "Workflow.h"
 
-int main() {
-    Workflow w;
-    w.work("input.txt");
+
+int main(int argc, char ** argv) {
+    if (argc != 2){
+        throw std::invalid_argument("Wrong arguments amount!");
+    }
+    Workflow wf(argv[1]);
+    wf.execute();
     return 0;
 }
