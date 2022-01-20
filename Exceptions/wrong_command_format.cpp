770bba0951ddc9b27ce9affb55b680469d8e703b
AliceDarkmoon55 <44077088+AliceDarkmoon55@users.noreply.github.com>
2022-01-20 18:45:52 +0700

    Create wrong_command_format.cpp

---
+++ Exceptions/wrong_command_format.cpp
@@ -0,0 +1,8 @@
+#include "wrong_command_format.h"
+
+error::wrong_command_format::wrong_command_format(const std::string &error_message) : err_msg(error_message)
+{}
+
+const char *error::wrong_command_format::what() const noexcept {
+    return err_msg.c_str();
+}
